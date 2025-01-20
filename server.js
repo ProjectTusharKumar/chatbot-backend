a799ba7b0e99a7fd8e4c439fd477fa2be48c89d2
ProjectTusharKumar <onlinetus4ar@gmail.com>
2025-01-20 18:52:43 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const db = require("./db");
+const db = require("./New folder/db");
 
 const app = express();
 app.use(bodyParser.json());
